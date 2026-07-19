5506f24fb4e2b48563566acd4a7fe34ca023ab84
IlliaDir <illia.dobrovolskyi2@gmail.com>
2026-07-19 14:18:22 +0300

    BookRepository replaced with JpaRepository, added liquibase and required endpoints, implemented soft delete

--- src/main/java/com/example/app/mapper/BookMapper.java
+++ src/main/java/com/example/app/mapper/BookMapper.java
@@ -4,11 +4,19 @@ import com.example.app.config.BookMapperConfig;
 import com.example.app.dto.BookDto;
 import com.example.app.dto.CreateBookRequestDto;
 import com.example.app.model.Book;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(config = BookMapperConfig.class)
 public interface BookMapper {
     BookDto toDto(Book book);
 
     Book toModel(CreateBookRequestDto bookDto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    void updateBook(CreateBookRequestDto requestDto, @MappingTarget Book book);
 }
